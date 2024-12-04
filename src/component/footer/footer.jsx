719f596687c3aa6022211ea943a04851dc5d982f
Sourav Jha <souravjha20212156@gmail.com>
2024-12-04 16:34:03 +0530

    initial commit

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+    return (
+        <div className="footer-title footer-font">
+        <p>Copyright © 2007 - 2023 - Free CSS. All Rights Reserved</p>
+        </div>
+    );
+}
